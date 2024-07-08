f680e51e23bfb6a85fe176d97e1a9273f117d052
porqueEUprogramo <porqueeuprogramo@gmail.com>
2024-07-08 23:58:51 +0100

    creation of eks with vpc and subnets on az

--- eks.tf
+++ eks.tf
@@ -1,9 +1,72 @@
+resource "aws_security_group" "eks_cluster_sg" {
+  name        = "eks-cluster-sg"
+  description = "EKS Cluster security group"
+  vpc_id      = aws_vpc.vpc_pep.id
+
+  // Allow inbound traffic from the EKS control plane on port 443 (HTTPS)
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [
+      "18.130.0.0/17",
+      "35.178.0.0/16",
+      "52.56.0.0/16",
+      "52.95.255.0/24"
+    ]
+  }
+
+  // Allow all traffic within the security group (nodes need to communicate with each other)
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+  }
+
+  // Allow outbound traffic to the internet for updates and external communication
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "eks_nodes_inbound" {
+  type              = "ingress"
+  from_port         = 1025
+  to_port           = 65535
+  protocol          = "tcp"
+  security_group_id = aws_security_group.eks_cluster_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "eks_nodes_outbound" {
+  type              = "egress"
+  from_port         = 1025
+  to_port           = 65535
+  protocol          = "tcp"
+  security_group_id = aws_security_group.eks_cluster_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "eks_cluster_api_server" {
+  type                      = "ingress"
+  from_port                 = 443
+  to_port                   = 443
+  protocol                  = "tcp"
+  security_group_id         = aws_security_group.eks_cluster_sg.id
+  source_security_group_id  = aws_security_group.eks_cluster_sg.id
+}
+
 resource "aws_eks_cluster" "eks_cluster_pep" {
   name     = "eks_cluster_pep"
   role_arn = aws_iam_role.iam_role_pep.arn
 
   vpc_config {
     subnet_ids = [aws_subnet.subnet_a_pep.id, aws_subnet.subnet_b_pep.id]
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
@@ -27,4 +90,6 @@ resource "aws_eks_node_group" "eks_node_group_pep" {
   }
 
   instance_types = ["t2.micro"]
+
+  ami_type = "AL2_x86_64"
 }
