7b7e53ef75578c34ce99b05ea71ceae40dbe3752
porqueEUprogramo <porqueeuprogramo@gmail.com>
2024-07-10 20:45:02 +0100

    creation of eks with vpc and subnets on az

--- phase2/eks.tf
+++ phase2/eks.tf
@@ -1,71 +1,62 @@
 locals {
-  role_arn = data.terraform_remote_state.terraform-state-phase1.outputs.iam_role_arn_output
-  role_name = data.terraform_remote_state.terraform-state-phase1.outputs.iam_role_name_output
-  subnet_a_pep_id = data.terraform_remote_state.terraform-state-phase1.outputs.subnet_a_pep_output
-  subnet_b_pep_id = data.terraform_remote_state.terraform-state-phase1.outputs.subnet_b_pep_output
-  eks_cluster_sg_pep_id = data.terraform_remote_state.terraform-state-phase1.outputs.eks_cluster_sg_pep_id_output
-  iam_role_node_group_arn = data.terraform_remote_state.terraform-state-phase1.outputs.iam_role_node_group_arn_output
-  iam_role_node_group_name = data.terraform_remote_state.terraform-state-phase1.outputs.iam_role_node_group_name_output
+  region = "us-east-1"
+  name   = "amonkincloud-cluster"
+  vpc_cidr = "10.123.0.0/16"
+  azs      = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
+  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  tags = {
+    Example = local.name
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = local.role_name
-}
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.1"
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = local.role_name
-}
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = local.iam_role_node_group_name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = local.iam_role_node_group_name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = local.iam_role_node_group_name
-}
-
-resource "aws_eks_cluster" "eks_cluster_pep" {
-  name     = "eks_cluster_pep"
-  role_arn = local.role_arn
-
-  vpc_config {
-    subnet_ids = [local.subnet_a_pep_id, local.subnet_b_pep_id]
-    security_group_ids = [local.eks_cluster_sg_pep_id]
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy
-  ]
-}
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
 
-resource "aws_eks_node_group" "eks_node_group_pep" {
-  cluster_name    = aws_eks_cluster.eks_cluster_pep.name
-  node_group_name = "eks_node_group_pep"
-  node_role_arn   = local.iam_role_node_group_arn
-  subnet_ids      = [local.subnet_a_pep_id, local.subnet_b_pep_id]
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["m5.large"]
 
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
+    attach_cluster_primary_security_group = true
   }
 
-  instance_types = ["t2.micro"]
+  eks_managed_node_groups = {
+    amc-cluster-wg = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
 
-  ami_type = "AL2_x86_64"
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+
+      tags = {
+        ExtraTag = "helloworld"
+      }
+    }
+  }
+
+  tags = local.tags
 }
+
