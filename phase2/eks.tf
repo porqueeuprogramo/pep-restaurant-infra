9de5e7db6725615f0ba76711b39ff90eb3695618
porqueEUprogramo <porqueeuprogramo@gmail.com>
2024-07-10 20:51:09 +0100

    creation of eks with vpc and subnets on az

--- phase2/eks.tf
+++ phase2/eks.tf
@@ -1,15 +1,4 @@
-locals {
-  region = "us-east-1"
-  name   = "amonkincloud-cluster"
-  vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
-  tags = {
-    Example = local.name
-  }
-}
+
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -37,7 +26,7 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
+    instance_types = ["t2.micro"]
 
     attach_cluster_primary_security_group = true
   }
@@ -48,11 +37,11 @@ module "eks" {
       max_size     = 2
       desired_size = 1
 
-      instance_types = ["t3.large"]
+      instance_types = ["t2.micro"]
       capacity_type  = "SPOT"
 
       tags = {
-        ExtraTag = "helloworld"
+        ExtraTag = "pep"
       }
     }
   }
