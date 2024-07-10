d1fb7c18405263ac441d49daaccfcb3ff835a5a8
Tiago Martins <tiago.am.martins@ctw.bmwgroup.com>
2024-07-10 16:37:46 +0100

    eks pod run

--- phase1/vpc.tf
+++ phase1/vpc.tf
@@ -12,10 +12,20 @@ resource "aws_subnet" "subnet_a_pep" {
   vpc_id            = aws_vpc.vpc_pep.id
   cidr_block        = "10.0.0.0/25"
   availability_zone = "eu-west-2a"
+
+  tags = {
+    "kubernetes.io/cluster/eks_cluster_pep" = "shared"
+    "kubernetes.io/role/internal-elb"       = "1"
+  }
 }
 
 resource "aws_subnet" "subnet_b_pep" {
   vpc_id            = aws_vpc.vpc_pep.id
   cidr_block        = "10.0.0.128/25"
   availability_zone = "eu-west-2b"
+
+  tags = {
+    "kubernetes.io/cluster/eks_cluster_pep" = "shared"
+    "kubernetes.io/role/internal-elb"       = "1"
+  }
 }
