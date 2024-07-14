0853230f74f1c42dc5fc03d4d9dc7d50cf4c3103
Tiago Martins <tiago.am.martins@ctw.bmwgroup.com>
2024-07-14 23:07:51 +0100

    eks pod run

--- phase2/main.tf
+++ phase2/main.tf
@@ -24,7 +24,7 @@ locals {
   vpc_intra_subnets_output              = data.terraform_remote_state.terraform-state-phase1.outputs.vpc_intra_subnets_output
   vpc_id_output                         = data.terraform_remote_state.terraform-state-phase1.outputs.vpc_id_output
   pep_db_enhanced_monitoring_arn_output = data.terraform_remote_state.terraform-state-phase1.outputs.pep_db_enhanced_monitoring_arn_output
-  pep-restaurant-ms-manager-db-endpoint = trim(module.pep-restaurant-ms-manager-db.this_db_instance_endpoint,":${module.pep-restaurant-ms-manager-db.this_db_instance_port}")
+  pep-restaurant-ms-manager-db-endpoint = replace(module.pep-restaurant-ms-manager-db.address, ":${module.pep-restaurant-ms-manager-db.port}", "")
   tags = {
     Rds = "rds"
   }
