4f5bec156f9008471ac4af313823174c9070e5e1
porqueEUprogramo <porqueeuprogramo@gmail.com>
2024-07-14 17:46:19 +0100

    creation of eks with vpc and subnets on az

--- phase1/outputs.tf
+++ phase1/outputs.tf
@@ -2,10 +2,6 @@ output "aws_security_group_db_id_output" {
   value = aws_security_group.db.id
 }
 
-output "aws_security_group_eks_id_output" {
-  value = aws_security_group.eks.id
-}
-
 output "vpc_private_subnets_output" {
   value = module.vpc.private_subnets
 }
--- phase1/securityGroup.tf
+++ phase1/securityGroup.tf
@@ -1,25 +1,3 @@
-resource "aws_security_group" "eks" {
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "pep-eks-sg"
-  }
-}
-
 resource "aws_security_group" "db" {
   vpc_id = module.vpc.vpc_id
 
