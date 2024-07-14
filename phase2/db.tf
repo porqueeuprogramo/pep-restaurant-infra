fe8ba8e0f103953e7af370501472bd3e79fd3aea
Tiago Martins <tiago.am.martins@ctw.bmwgroup.com>
2024-07-14 22:14:05 +0100

    eks pod run

--- phase2/db.tf
+++ phase2/db.tf
@@ -18,7 +18,8 @@ resource "aws_ssm_parameter" "pep-restaurant-ms-manager-db-password" {
 
 # https://github.com/terraform-aws-modules/terraform-aws-rds
 module "pep-restaurant-ms-manager-db" {
-  source  = "../plugins/terraform-aws-modules/terraform-aws-rds-2.24.0"
+  source                              = "terraform-aws-modules/rds/aws"
+  version                             = "2.24.0"
   identifier                          = local.pep-restaurant-ms-manager-id
   engine                              = "postgres"
   engine_version                      = "12"
