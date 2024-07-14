f7cfdf0475b8d5e92a0020937b8ded3e0cfa296a
porqueEUprogramo <porqueeuprogramo@gmail.com>
2024-07-14 14:52:03 +0100

    creation of eks with vpc and subnets on az

--- phase2/main.tf
+++ phase2/main.tf
@@ -12,19 +12,3 @@ data "terraform_remote_state" "terraform-state-phase1" {
     key    = "phase1/terraform.tfstate"
   }
 }
-
-locals {
-  pep-restaurant-ms-manager-id          = "pep-restaurant-ms-manager-id"
-  pep-restaurant-ms-manager-db-name     = "PepRestaurantMsManagerDb"
-  pep-restaurant-ms-manager-db-username = "porqueeuprogramo"
-  pep-restaurant-ms-manager-db-port     = 5432
-  aws_security_group_db_id              = data.terraform_remote_state.terraform-state-phase1.outputs.aws_security_group_db_id_output
-  aws_security_group_eks_id_output      = data.terraform_remote_state.terraform-state-phase1.outputs.aws_security_group_eks_id_output
-  vpc_private_subnets                   = data.terraform_remote_state.terraform-state-phase1.outputs.vpc_private_subnets_output
-  vpc_public_subnets_output             = data.terraform_remote_state.terraform-state-phase1.outputs.vpc_public_subnets_output
-  vpc_intra_subnets_output              = data.terraform_remote_state.terraform-state-phase1.outputs.vpc_intra_subnets_output
-  vpc_id_output                         = data.terraform_remote_state.terraform-state-phase1.outputs.vpc_id_output
-  tags = {
-    Rds = "rds"
-  }
-}
