28165cd4594e1fc2111c5e6b8b5fc2fe69c0a0e3
porqueEUprogramo <porqueeuprogramo@gmail.com>
2024-07-15 20:37:20 +0100

    creation of eks with vpc and subnets on az

--- phase2/db.tf
+++ phase2/db.tf
@@ -3,18 +3,18 @@ resource "random_password" "pep-ms-restaurant-db-password" {
   special = false
 }
 
-resource "aws_ssm_parameter" "pep-restaurant-ms-manager-db-password" {
- name        = "/pep/restaurant/ms/manager/database/db-password"
- description = "DB Password for pep-restaurant-ms-manager"
- type        = "SecureString"
- value       = random_password.pep-ms-restaurant-db-password.result
- tags        = local.tags
- lifecycle {
-   ignore_changes = [
-     value
-   ]
- }
-}
+# resource "aws_ssm_parameter" "pep-restaurant-ms-manager-db-password" {
+#  name        = "/pep/restaurant/ms/manager/database/db-password"
+#  description = "DB Password for pep-restaurant-ms-manager"
+#  type        = "SecureString"
+#  value       = random_password.pep-ms-restaurant-db-password.result
+#  tags        = local.tags
+#  lifecycle {
+#    ignore_changes = [
+#      value
+#    ]
+#  }
+# }
 
 # Data source to retrieve a single subnet in eu-west-2a
 data "aws_subnet" "az_a" {
@@ -51,35 +51,35 @@ resource "aws_db_subnet_group" "aws_db_subnet_group" {
   }
 }
 
-resource "aws_db_instance" "pep-restaurant-ms-manager-db" {
-  identifier            = local.pep-restaurant-ms-manager-id
-  allocated_storage     = 200
-  engine                = "postgres"
-  engine_version        = "12"
-  instance_class        = "db.t3.medium"
-  db_name               = local.pep-restaurant-ms-manager-db-name
-  username              = local.pep-restaurant-ms-manager-db-username
-  password              = random_password.pep-ms-restaurant-db-password.result
-
-  # Enable IAM database authentication
-  iam_database_authentication_enabled = true
-
-  # Set the VPC security group IDs (example assumes you have a security group defined)
-  vpc_security_group_ids = [local.aws_security_group_db_id]
-
-  # Specify the subnet group where the DB instance will be deployed
-  db_subnet_group_name = aws_db_subnet_group.aws_db_subnet_group.id
-
-  # Backup and maintenance window configurations
-  backup_retention_period = 7
-  maintenance_window      = "Mon:00:00-Mon:03:00"
-
-  # Optionally enable deletion protection
-  deletion_protection = false
-
-  # Tags for identification
-  tags = {
-    Name = "pep-restaurant-ms-manager-db"
-    Environment = "Dev"
-  }
-}
+# resource "aws_db_instance" "pep-restaurant-ms-manager-db" {
+#   identifier            = local.pep-restaurant-ms-manager-id
+#   allocated_storage     = 200
+#   engine                = "postgres"
+#   engine_version        = "12"
+#   instance_class        = "db.t3.medium"
+#   db_name               = local.pep-restaurant-ms-manager-db-name
+#   username              = local.pep-restaurant-ms-manager-db-username
+#   password              = random_password.pep-ms-restaurant-db-password.result
+#
+#   # Enable IAM database authentication
+#   iam_database_authentication_enabled = true
+#
+#   # Set the VPC security group IDs (example assumes you have a security group defined)
+#   vpc_security_group_ids = [local.aws_security_group_db_id]
+#
+#   # Specify the subnet group where the DB instance will be deployed
+#   db_subnet_group_name = aws_db_subnet_group.aws_db_subnet_group.id
+#
+#   # Backup and maintenance window configurations
+#   backup_retention_period = 7
+#   maintenance_window      = "Mon:00:00-Mon:03:00"
+#
+#   # Optionally enable deletion protection
+#   deletion_protection = false
+#
+#   # Tags for identification
+#   tags = {
+#     Name = "pep-restaurant-ms-manager-db"
+#     Environment = "Dev"
+#   }
+# }
--- phase2/secrets.tf
+++ phase2/secrets.tf
@@ -1,18 +1,18 @@
-resource "aws_secretsmanager_secret" "pep-restaurant-ms-manager-db-secret" {
- description             = "Secret to store pep restaurant ms manager db password"
- name                    = "/pep/restaurant/ms/manager/database"
- recovery_window_in_days = 7
-}
-
-resource "aws_secretsmanager_secret_version" "pep-restaurant-ms-manager-db-secret-version" {
- secret_id = aws_secretsmanager_secret.pep-restaurant-ms-manager-db-secret.id
- secret_string = <<EOF
- {
-   "engine": "postgres",
-   "port": "${local.pep-restaurant-ms-manager-db-port}",
-   "username": "${local.pep-restaurant-ms-manager-db-username}",
-   "password": "${random_password.pep-ms-restaurant-db-password.result}",
-   "dbname": "${local.pep-restaurant-ms-manager-db-name}"
- }
-EOF
-}
+# resource "aws_secretsmanager_secret" "pep-restaurant-ms-manager-db-secret" {
+#  description             = "Secret to store pep restaurant ms manager db password"
+#  name                    = "/pep/restaurant/ms/manager/database"
+#  recovery_window_in_days = 7
+# }
+#
+# resource "aws_secretsmanager_secret_version" "pep-restaurant-ms-manager-db-secret-version" {
+#  secret_id = aws_secretsmanager_secret.pep-restaurant-ms-manager-db-secret.id
+#  secret_string = <<EOF
+#  {
+#    "engine": "postgres",
+#    "port": "${local.pep-restaurant-ms-manager-db-port}",
+#    "username": "${local.pep-restaurant-ms-manager-db-username}",
+#    "password": "${random_password.pep-ms-restaurant-db-password.result}",
+#    "dbname": "${local.pep-restaurant-ms-manager-db-name}"
+#  }
+# EOF
+# }
